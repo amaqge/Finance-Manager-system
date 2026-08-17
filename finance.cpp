06a8e6f57390ffac3651045d855cd16885b83d0b
Amage <serenkohamed@gmail.com>
2026-08-17 16:02:11 +0300

    Add expense categories

--- finance.cpp
+++ finance.cpp
@@ -38,9 +38,9 @@ public:
         file.close();
     }
 // the constructor of the class
-    void add_info(int data, string con, int c){
+    void add_info(int data, string con, int c, string category){
         file.open("finance.txt", ios::app);
-        file<<"Data; "<<data<<"| Consumption; "<<con<<"| Cost; "<<c<<endl;        
+        file<<"Data; "<<data<<"| Category; "<<category<<"| Consumption; "<<con<<"| Cost; "<<c<<endl;        
         file.close();
     }
 // the function to display the data from the file
@@ -54,11 +54,51 @@ public:
     }
 };
 
+
+class Category:public File{
+protected:
+// type of category and the cost of each category
+    string category;
+    int food = 0;
+    int transport = 0;
+    int entertainment = 0;
+    int health = 0;
+    int other = 0;
+public:
+    Category(){}
+    // the function to add the cost to the category
+    void type_category(string category, int cost){
+        this->category = category;
+        if(category == "food" || category == "Food"){
+            food += cost;
+        }
+        else if(category == "transport" || category == "Transport"){
+            transport += cost;
+        }
+        else if(category == "entertainment" || category == "Entertainment"){
+            entertainment += cost;
+        }
+        else if(category == "health" || category == "Health"){
+            health += cost;
+        }
+        else{
+            other += cost;
+        } 
+    }
+    void display_category(){
+        cout<<"Food: "<<food<<endl;
+        cout<<"Transport: "<<transport<<endl;
+        cout<<"Entertainment: "<<entertainment<<endl;
+        cout<<"Health: "<<health<<endl;
+        cout<<"Other: "<<other<<endl;
+    }
+};
+
 class Stats:public File{
 protected:
     int sum = 0;
 public:
-    // the constructor to calculate the total spending
+    // the function to calculate the total spending
     void total(int cost){
         sum += cost;
     }
@@ -71,6 +111,7 @@ public:
         int balance=income-sum;
         cout<<"Balance: "<<balance<<endl;
     }
+    
 };
 
 class Add_info:public File{
@@ -78,28 +119,23 @@ protected:
     int data;
     string consumption;
     int cost;
+    string category;
 public:
-    void add_info(int data, int cost, string consumption, Stats& s, File& f1){
+    void add_info(int data, int cost, string consumption, Stats& s, File& f1, Category& c, string category){
         // if you spent more than your income it will display an error message
         if(f1.get_income() - cost < 0){
             cout<<"ERROR : you cannot spent more than your income"<<endl;
         }
         // if you spent less than your income it will add the data to the file
         else{
-        f1.add_info(data, consumption, cost);
+        f1.add_info(data, consumption, cost, category);
         s.total(cost);
         f1.take_cost(cost);
+        c.type_category(category, cost);
         }
     }
     
 };
-
-
-
- 
-
-
-
 
 int finance(){
     int icome;
@@ -108,6 +144,7 @@ int finance(){
         File f1;
         Stats s;
         Add_info a;
+        Category c;
         f1.ho(icome);
     while(true){
         int choice;
@@ -115,10 +152,11 @@ int finance(){
         cout<<"||         Welcome to Finance Manager System.        ||"<<endl;
         cout<<"||        Please select an option:                   ||"<<endl;
         cout<<"|| 1. Add spending                                   ||"<<endl;
-        cout<<"|| 2. Display finance information                    ||"<<endl;
+        cout<<"|| 2. Display category information                   ||"<<endl;
         cout<<"|| 3. Display total spending                         ||"<<endl;
         cout<<"|| 4. Display balance                                ||"<<endl;
-        cout<<"|| 5. Exit                                           ||"<<endl;
+        cout<<"|| 5. Display all information from the file          ||"<<endl;
+        cout<<"|| 6. Exit                                           ||"<<endl;
         cout<<"======================================================"<<endl;
         cout<<"Enter your choice: ";
         cin>>choice;
@@ -126,18 +164,20 @@ int finance(){
         switch(choice){
            case 1:{
             int data,cost;
-            string consumption;
+            string consumption, category;
             cout<<"Enter the date: ";
             cin>>data;
+            cout<<"Enter the category: ";
+            cin>>category;
             cout<<"Enter the consumption: ";
             cin>>consumption;
             cout<<"Enter the cost: ";
             cin>>cost;
-            a.add_info(data, cost, consumption, s, f1);
+            a.add_info(data, cost, consumption, s, f1, c, category);
             break;
            }
            case 2:{
-            f1.display();
+            c.display_category();
             break;
            }
            case 3:{
@@ -149,6 +189,10 @@ int finance(){
             break;
            }
            case 5:{
+            f1.display();
+            break;
+           }
+           case 6:{
             cout<<"Exiting the program..."<<endl;
             return 0;
            }
